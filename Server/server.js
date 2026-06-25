630a2b7372610937dbc8ecbff8c330946194592d
AswiniSudhaKattamuri <aswinisudha49@gmail.com>
2026-06-25 19:03:08 +0530

    Implement JWT authentication and protected profile route

--- Server/server.js
+++ Server/server.js
@@ -8,9 +8,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
+const authRoutes = require("./routes/authRoutes");
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
