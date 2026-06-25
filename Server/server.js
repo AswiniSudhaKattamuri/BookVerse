6ade22f8b929913f6afacd763962022ff19837e7
AswiniSudhaKattamuri <aswinisudha49@gmail.com>
2026-06-25 17:53:11 +0530

    Setup backend and ignore environment file

---
+++ Server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 BookVerse API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
